437e22e887f74ba81974e6ca3b7a7260d5bb08f3
SANGHYUN <norimsu@gmail.com>
2023-01-24 20:01:52 +0900

    feat: front controller

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,16 +20,23 @@ public class HelloBootApplication {
     public static void main(String[] args) {
         final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp)
                         throws ServletException, IOException {
-                    final String name = req.getParameter("name");
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello, " + name + "!");
+                    // 인증, 보안, 다국어, 공통 처리를 한다고 가정한다.
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        final String name = req.getParameter("name");
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello, " + name + "!");
+                    } else if (req.getRequestURI().equals("/user")) {
+                        // 사용자 정보를 조회한다고 가정한다.
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start();
     }
