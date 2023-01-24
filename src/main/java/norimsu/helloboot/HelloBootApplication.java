0d0ce9789328b151a545db09cd75246aa1294537
SANGHYUN <norimsu@gmail.com>
2023-01-24 21:16:23 +0900

    refactor: container java configuration

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -3,6 +3,7 @@ package norimsu.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,15 +13,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class HelloBootApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
         final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+                final ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                final DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                //dispatcherServlet.setApplicationContext(this); // ApplicationContextAware
+
                 final WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
                         "dispatcherServlet",
-                        new DispatcherServlet(this)).addMapping("/*"));
+                        dispatcherServlet).addMapping("/*"));
                 webServer.start();
             }
         };
