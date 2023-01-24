14c1a04451934aebf6b88aa45616931e96ed1ba6
SANGHYUN <norimsu@gmail.com>
2023-01-24 21:24:35 +0900

    refactor: SpringApplication

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -1,5 +1,6 @@
 package norimsu.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +23,7 @@ public class HelloBootApplication {
     }
 
     public static void main(String[] args) {
-        MySpringApplication.run(HelloBootApplication.class, args);
+        SpringApplication.run(HelloBootApplication.class, args);
     }
 
 }
