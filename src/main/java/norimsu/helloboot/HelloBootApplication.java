ba9f3675395649e0772176ddff8650bdbc39f8f9
SANGHYUN <norimsu@gmail.com>
2023-01-24 23:33:16 +0900

    refactor: configuration class

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -1,23 +1,9 @@
 package norimsu.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @MySpringBootApplication
 public class HelloBootApplication {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloBootApplication.class, args);
