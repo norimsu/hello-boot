13a22e625c2d627654689ab0517d35eba0f4fd6b
SANGHYUN <norimsu@gmail.com>
2023-01-24 19:56:06 +0900

    refactor: replace constant value

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -2,7 +2,6 @@ package norimsu.helloboot;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class HelloBootApplication {
 
@@ -22,8 +23,8 @@ public class HelloBootApplication {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp)
                         throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
                     resp.getWriter().println("Hello, Servlet!");
                 }
             }).addMapping("/hello");
