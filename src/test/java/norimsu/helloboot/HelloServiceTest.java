601934f7173f874546ae13c0a919c6e257cb598b
SANGHYUN <norimsu@gmail.com>
2023-01-24 23:31:15 +0900

    refactor: composite annotation

--- src/test/java/norimsu/helloboot/HelloServiceTest.java
+++ src/test/java/norimsu/helloboot/HelloServiceTest.java
@@ -3,18 +3,37 @@ package norimsu.helloboot;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.junit.jupiter.api.Test;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest {
+
+}
 
 class HelloServiceTest {
 
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         HelloService helloService = new SimpleHelloService();
         final String ret = helloService.sayHello("Test");
         assertThat(ret).isEqualTo("Hello, Test!");
     }
 
-    @Test
+    @UnitTest
     void helloDecorator() {
         HelloService helloService = new HelloDecorator(name -> name);
         final String ret = helloService.sayHello("Test");
