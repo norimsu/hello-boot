1afd521b6253f5d6749492f78d66869c55da1f5d
SANGHYUN <norimsu@gmail.com>
2023-01-24 19:26:13 +0900

    first commit

---
+++ src/test/java/norimsu/helloboot/HelloBootApplicationTests.java
@@ -0,0 +1,13 @@
+package norimsu.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
