18b1d071dd1b22ac656693e56f13be818289f2f6
SANGHYUN <norimsu@gmail.com>
2023-01-24 20:46:45 +0900

    refactor: integration spring

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -9,16 +9,20 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HelloBootApplication {
 
     public static void main(String[] args) {
-        final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                final WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
+                        "dispatcherServlet",
+                        new DispatcherServlet(this)).addMapping("/*"));
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-        final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        final WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
-                "dispatcherServlet",
-                new DispatcherServlet(applicationContext)).addMapping("/*"));
-        webServer.start();
     }
 
 }
