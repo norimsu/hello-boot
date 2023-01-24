5160ee578de2ba12cfb2993587a05901c6ea8636
SANGHYUN <norimsu@gmail.com>
2023-01-25 03:15:20 +0900

    refactor: Custom Conditional Annotation

---
+++ src/main/java/norimsu/config/ConditionalMyOnClass.java
@@ -0,0 +1,16 @@
+package norimsu.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Conditional;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Conditional(MyOnClassCondition.class)
+public @interface ConditionalMyOnClass {
+
+    String value();
+}
---
+++ src/main/java/norimsu/config/MyOnClassCondition.java
@@ -0,0 +1,19 @@
+package norimsu.config;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
+
+public class MyOnClassCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        final Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(ConditionalMyOnClass.class.getName());
+        final String value = (String) annotationAttributes.get("value");
+        return ClassUtils.isPresent(value, context.getClassLoader());
+
+    }
+}
