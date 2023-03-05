7b7633d6a298180159f34818013d92388e3975c0
SANGHYUN <norimsu@gmail.com>
2023-03-06 02:01:18 +0900

    refactor: Modify user configuration to override auto configuration

--- src/main/java/norimsu/boot/configure/TomcatWebServerConfig.java
+++ src/main/java/norimsu/boot/configure/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package norimsu.boot.configure;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import norimsu.boot.MyAutoConfiguration;
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
