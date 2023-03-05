8b18458d355f332f469a56372f0b72b0737c38ee
SANGHYUN <norimsu@gmail.com>
2023-03-06 02:41:05 +0900

    refactor: revert

--- src/main/java/norimsu/helloboot/HelloBootApplication.java
+++ src/main/java/norimsu/helloboot/HelloBootApplication.java
@@ -1,9 +1,6 @@
 package norimsu.helloboot;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 import norimsu.boot.autoconfigure.MySpringBootApplication;
 
@@ -12,14 +9,6 @@ public class HelloBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloBootApplication.class, args);
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner(Environment env) {
-        return args -> {
-            final String name = env.getProperty("my.name");
-            System.out.println("name = " + name);
-        };
     }
 
 }
