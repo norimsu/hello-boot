a9ec3b2287a5ede5bdcf55fc9ecef69b86543f8a
SANGHYUN <norimsu@gmail.com>
2023-03-06 03:06:29 +0900

    refactor: Add ServerProperties and Bean

---
+++ src/main/java/norimsu/boot/autoconfigure/embeded/ServerProperties.java
@@ -0,0 +1,25 @@
+package norimsu.boot.autoconfigure.embeded;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ServerProperties {
+
+    private String contextPath;
+    private int port;
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
--- src/main/java/norimsu/boot/autoconfigure/embeded/TomcatWebServerConfig.java
+++ src/main/java/norimsu/boot/autoconfigure/embeded/TomcatWebServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import norimsu.boot.autoconfigure.MyAutoConfiguration;
 import norimsu.boot.autoconfigure.condition.ConditionalMyOnClass;
@@ -13,16 +14,20 @@ import norimsu.boot.autoconfigure.condition.ConditionalMyOnClass;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-    @Value("${contextPath:}") String contextPath;
-    @Value("${port:8080}") int port;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         final TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        System.out.println("contextPath = " + contextPath);
-        factory.setContextPath(this.contextPath);
-        factory.setPort(this.port);
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
         return factory;
+    }
+
+    @Bean
+    public ServerProperties serverProperties(Environment env) {
+        final ServerProperties properties = new ServerProperties();
+        properties.setContextPath(env.getProperty("contextPath", ""));
+        properties.setPort(env.getProperty("port", Integer.class, 8080));
+        return properties;
     }
 }
